3b9c9fd27de94dd06f203b9a8fbcce1527247d8c
DmiE <Dawid.Mieszczak95@gmail.com>
2020-09-10 15:28:20 +0200

    fix scroll

--- src/js/index.js
+++ src/js/index.js
@@ -7,18 +7,16 @@ import '../../node_modules/aos/dist/aos.css';
 import ImageGalleryCreator from './gallery';
 
 $(document).ready(function() {
-  AOS.init();
-
+  // AOS.init();
   $('.jumbotron__slider').slick({
     infinite: true,
     slidesToShow: 1,
     draggable: true,
     autoplay: true,
-    autoplaySpeed: 3000
+    autoplaySpeed: 3000,
+    arrows: false
   });
-
   // MENU
-
   window.addEventListener('scroll', () => {
     if (window.scrollY > window.innerHeight - 100) {
       $('#header').addClass('header__scrolled');
@@ -26,63 +24,50 @@ $(document).ready(function() {
       $('#header').removeClass('header__scrolled');
     }
   });
-
-  // icon click
-
+  // ICON CLICK
   let clickDisabled = false;
   $('#nav-icon4').on('click', () => {
     if (!clickDisabled) {
       $('#nav-icon4').toggleClass('open');
       $('#mobile-navigation').toggleClass('menu_opened');
-      $('body').toggleClass('scroll__block');
-
+      $('html').toggleClass('scroll__block');
       clickDisabled = true;
       setTimeout(function() {
         clickDisabled = false;
       }, 500);
     }
   });
-
   $('.mobile-navigation__item').on('click', () => {
     $('#nav-icon4').removeClass('open');
     $('#mobile-navigation').removeClass('menu_opened');
-    $('body').removeClass('scroll__block');
+    $('html').removeClass('scroll__block');
   });
-
   $(window).resize(() => {
     if ($(window).width() >= 1024) {
       $('#nav-icon4').removeClass('open');
       $('#mobile-navigation').removeClass('menu_opened');
-      $('body').removeClass('scroll__block');
+      $('html').removeClass('scroll__block');
     }
   });
-
-  // HEADER COLOR CHANGE
-
-  // SMOOTH SCROLL
-
+  // // HEADER COLOR CHANGE
+  // // SMOOTH SCROLL
   document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
       e.preventDefault();
-
       document.querySelector(this.getAttribute('href')).scrollIntoView({
         behavior: 'smooth'
       });
     });
   });
-
-  //REFACTOR ^^^^
-
+  // //REFACTOR ^^^^
   const imageMap = {
     mapItems: document.querySelectorAll('.image_map__item'),
     tableRows: document.querySelectorAll('.main-table__row'),
-
     setListeners: function() {
       imageMap.mapItems.forEach(element => {
         element.addEventListener('mousemove', e => {
           console.log('jezdze :D');
         });
-
         element.addEventListener('mouseenter', e => {
           this.tableRows.forEach(element => {
             if (element.dataset.houseNumber === e.target.dataset.houseNumber) {
@@ -91,13 +76,10 @@ $(document).ready(function() {
         });
       });
     },
-
     setCloudInfo: function() {},
     readDataFromTable: function() {}
   };
-
-  // imageMap.setListeners();
-
+  imageMap.setListeners();
   const photoGallery = new ImageGalleryCreator('photo_gallery');
   photoGallery.init();
 });
